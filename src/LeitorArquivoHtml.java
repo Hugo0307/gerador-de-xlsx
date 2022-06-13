887f898609706e196909d673c6a432d2c1e16f1b
hhenrique <hugohenrique20@gmail.com>
2022-06-12 22:55:12 -0300

    refatorando classe LeitorArquivoHtml

--- src/LeitorArquivoHtml.java
+++ src/LeitorArquivoHtml.java
@@ -9,81 +9,123 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class LeitorArquivoHtml {
-	
-	public static void main(String[] args) {
 		
-		String nomeRamo = "";
-		String dataHora = "";
-		String fonteLinha = "";
-		boolean linhaAnteriorIsTagA = false;
-		ArrayList<String> fontes = new ArrayList<String>();
+		private String nomeRamo;
+		private String dataHora;
+		private String fonteLinha;
+		private boolean linhaAnteriorIsTagA;
+		private ArrayList<String> fontes = new ArrayList<String>();
 		
-		try {
-			String pathFileHtml = JOptionPane.showInputDialog("Informe o path do arquivo HTML. \nExemplo: C:/Users/Hugo/Documents/VCMUpdateReport2022-06-03T17-57-54Z.html");
-			
-			BufferedReader bReader = 
-					new BufferedReader(new FileReader(pathFileHtml));
-			
-			while (bReader.ready()) {
-				/*lendo e obtendo linha por linha */
-				String linha = bReader.readLine();
+		public void getLeitor() {
+		
+			try {
+				BufferedReader bReader = 
+						new BufferedReader(new FileReader(getArquivo()));
 				
-				/* obtendo a linha que contém o nome do ramo */
-				if (linha.contains(" TCAP/")) 
-					nomeRamo = linha;
-				
-				/* obtendo a linha que contém a data e hora */
-				if (linha.contains(" BRT")) 
-					dataHora = linha;
-				
-				/* verifica se alinha anterior foi a tag a */
-				if (linhaAnteriorIsTagA) {
+				while (bReader.ready()) {
+					/*lendo e obtendo linha por linha */
+					String linha = bReader.readLine();
 					
-					/* despreza as linhas que não é nome de fonte */
-					if (!linha.contains("Rebase") && !linha.contains("/TCAP")) {
-						/* só adiciona à lista se o nome do fonte adicionado anteriormente não for igual */ 
-						if (!linha.equals(fonteLinha)) {
-							fontes.add(linha.trim());
+					/* obtendo a linha que contém o nome do ramo */
+					if (linha.contains(" TCAP/")) 
+						nomeRamo = linha;
+					
+					/* obtendo a linha que contém a data e hora */
+					if (linha.contains(" BRT")) 
+						dataHora = linha;
+					
+					/* verifica se alinha anterior foi a tag a */
+					if (linhaAnteriorIsTagA) {
+						
+						/* despreza as linhas que não é nome de fonte */
+						if (!linha.contains("Rebase") && !linha.contains("/TCAP")) {
+							/* só adiciona à lista se o nome do fonte adicionado anteriormente não for igual */ 
+							if (!linha.equals(fonteLinha)) {
+								fontes.add(linha.trim());
+							}
+							fonteLinha = linha;
 						}
-						fonteLinha = linha;
+						linhaAnteriorIsTagA = false;
 					}
-					linhaAnteriorIsTagA = false;
+					
+					/* verifica se contém a tag a na linha */
+					if (linha.contains("<a "))
+						linhaAnteriorIsTagA = true;
 				}
+				bReader.close();
 				
-				/* verifica se contém a tag a na linha */
-				if (linha.contains("<a "))
-					linhaAnteriorIsTagA = true;
-				
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
+				e.printStackTrace();
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
+				e.printStackTrace();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
+				e.printStackTrace();
 			}
-			/* INICIO - Somente para exibição no console do que está sendo escrito no arquivo excel */
-			nomeRamo = nomeRamo.substring(22);
-			System.out.println("Nome do ramo: " + nomeRamo);
 			
-			dataHora = dataHora.substring(5, 26);
-			System.out.println("Data/Hora: " + dataHora);
-			
-			System.out.println("FONTES:");
-			
-			/* percorrendo lista de fontes e imprimindo cada um sem espaços antes nem depois*/
-			for (String fonte : fontes)
-				System.out.println(fonte.trim());
-			/* FIM - Somente para exibição no console do que está sendo escrito no arquivo excel */
-			
-			bReader.close();
-			
-		} catch (FileNotFoundException e) {
-			JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
-			e.printStackTrace();
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e + " - " + e.getMessage());
-			e.printStackTrace();
+			GeradorXsl.escreverXsl(nomeRamo, dataHora, fontes);
 		}
 		
-		GeradorXsl.escreverXsl(nomeRamo, dataHora, fontes);
+		public String getArquivo() {
+			
+			boolean continua = true;
+			String pathFileHtml = "";
+			
+			while(continua) {
+				
+				pathFileHtml = JOptionPane.showInputDialog(
+						"Informe o path do arquivo HTML. \nExemplo: C:/Users/Hugo/Documents/VCMUpdateReport2022-06-03T17-57-54Z.html");
+				if(pathFileHtml.isBlank()) {
+					JOptionPane.showMessageDialog(null, "Campo não pode ser vazio. Tente novamente!");
+					continue;
+				} else if(!pathFileHtml.contains(".html")) {
+					pathFileHtml += ".html";
+				}
+				
+				String addMaisUm = JOptionPane.showInputDialog(
+						"Deseja informar mais? \nS - Sim | N - Não");
+
+				if(addMaisUm.equalsIgnoreCase("s")) {
+					continue;
+				} else {
+					continua = false;
+				}
+			
+			}
+			
+			return pathFileHtml;
+		}
 		
-	}
+		public void getExibirConsole(String nomeRamo, String dataEHora, ArrayList<String> fontes ) {
+			
+			if(nomeRamo != null) {
+				nomeRamo = nomeRamo.substring(22);
+				System.out.println("Nome do ramo: " + nomeRamo);
+			} else {
+				throw new NullPointerException(
+						"Nome do ramo não pode ser nulo!");
+			}
+			if(dataEHora != null) {
+				dataEHora = dataEHora.substring(5, 26);
+				System.out.println("Data/Hora: " + dataEHora);
+			} else {
+				throw new NullPointerException(
+						"Data e hora não podem ser nulos!");
+			}
+			if(!fontes.isEmpty()) {
+				System.out.println("FONTES:");
+				
+				/* percorrendo lista de fontes e imprimindo cada um sem espaços antes nem depois*/
+				for (String fonte : fontes) {
+					if(!fonte.equals(null))
+						System.out.println(fonte.trim());
+				}
+			} else {
+				throw new IllegalArgumentException(
+						"A lista de arquivos fonte está vazia!");
+			}
+		}
 
 }
